8e67a3a828f5ce383ae0b960036602c509a46db6
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-04-22 12:29:43 -0400

    connected article and category, and added validations and scopes to article'

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ActiveRecord::Base
+    belongs_to :category
+
+    validates, :title, :content, presence: true
+
+    scope :alphabetical, -> { order(:title) }
+    scope :active, -> { where(active: true) }
 end
