87c00ea413ec786f3214d9b83691080c3b1b4d06
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-04-22 13:16:00 -0400

    added routes validations and scopeS

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ActiveRecord::Base
+
+    belongs_to :category
+
+    validates_prescence_of :content, :title
+
+    scope :alphabetical, -> { order(:name) }
+    scope :active, -> { where(active: true) }
 end
